4e40b75a70b02e621a91c46376365b40cdeea934
Khoa hoc du lieu 17A2 <cdthang@uneti.edu.vn>
2023-11-11 19:22:49 +0700

    bai tap

---
+++ chuong6bai5.py
@@ -0,0 +1,7 @@
+so_luong=int(input("Nhập số lượng :"))
+don_gia=int(input("Nhập đơn giá :"))
+print("Thành tiền =",so_luong,"*",don_gia,"=",so_luong*don_gia)
+#1.7
+C=int(input("Nhập độ C :"))
+F=(C*9/5)+32
+print(C,"độ C","=",F,"độ F")
